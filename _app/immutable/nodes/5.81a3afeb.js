0f397ba391062850ed2e93b6efb280225e3ba2b9
seo-rii <me@seorii.page>
2023-09-20 13:10:09 +0900

    Updates

---
+++ _app/immutable/nodes/5.81a3afeb.js
@@ -0,0 +1,1 @@
+import{S as o,i as r,s as i,q as c,r as l,b as m,H as a,h as p}from"../chunks/index.c1272f9f.js";function u(s){let t;return{c(){t=c("아직 경기 전이에요.")},l(e){t=l(e,"아직 경기 전이에요.")},m(e,n){m(e,t,n)},p:a,i:a,o:a,d(e){e&&p(t)}}}class x extends o{constructor(t){super(),r(this,t,null,u,i,{})}}export{x as component};
