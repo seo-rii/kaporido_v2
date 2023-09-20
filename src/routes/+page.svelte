ea79d299a05e9e8da7be3368f51f2fef68a8c002
Seohyun Lee <studioseorii@gmail.com>
2023-09-21 01:03:14 +0900

    feat: add test2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h2>테스트 경기</h2>
+<h2>테스트 1</h2>
 {#each {length: 5} as _, i}
-    <p><a href="/kaporido_v2/test/r{i+1}">라운드 {i + 1}</a></p>
+    <p><a href="/kaporido_v2/test/r{i+1}?fast">라운드 {i + 1}</a></p>
+{/each}
+<h2>테스트 2</h2>
+{#each {length: 2} as _, i}
+    <p><a href="/kaporido_v2/t2/r{i+1}?fast">라운드 {i + 1}</a></p>
 {/each}
